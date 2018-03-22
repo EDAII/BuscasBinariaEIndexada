b60f4e28d8f6e82aaac47cc6fe4b0bbaf81c0d60
RomuloSouza <romulovinicius10@gmail.com>
2018-03-22 16:09:00 -0300

    Merged indexada.cpp in tempo.cpp

--- tempo.cpp
+++ tempo.cpp
@@ -34,43 +34,94 @@ bool buscaBinaria (int x, int n, int v[]) {
 int numeroAleatorio(int menor, int maior) {
   return rand()%(maior-menor+1) + menor;
 }
-/*
-int funcao(int x, int y,int z){
-int i,j;
-for(i=0;i<x;i++){
-	for(j=0;j<y;j++){
-		z++;
+
+int buscaIndexada(int num, int vetor[], int tabela_de_indices[], int aux){
+  int i=0,j=0;
+  if(num ==0){
+    return 0;
+  }
+  while(num > tabela_de_indices[i] && i<(aux/4)){
+    i++;
+  }
+  if(num == tabela_de_indices[i]){
+    return 1;
+  }
+  else if(num < tabela_de_indices[i]){
+    j = (i*(aux/4));
+    while(num < vetor[j]){
+      j--;
+    }
+    if(num ==vetor[j]){
+      return 1;
+    }
+    else{
+      return 0;
+    }
+  }
 }
+
+void preenche_tabela(int vetor[], int aux, int tabela_de_indices[]){
+  int i=0;
+
+  while(i < (aux/4)){
+    tabela_de_indices[i] = vetor[i*(aux/4)];
+    i++;
+  }
 }
-return z;
-}
-*/
 
 int main(){
 
 struct rusage tempo_inicial, tempo_final;
 double tempo;
-int z;
-int aux = 1000000;
+int aux;
+int num;
+
+cout << "Informe o tamanho do vetor (caso menor que 20, os valores irão aparecer no console) = ";
+cin >> aux;
+
+int tabela_de_indices[aux/4];
 int vetor[aux];
 
+  for (int i=0; i < aux; i++){
+    vetor[i] = numeroAleatorio(0, 10000);
+  }
+  sort (vetor, vetor+aux);
 
-getrusage(RUSAGE_SELF, &tempo_inicial);
-  //   z = funcao(10000,10000,z);
-    for (int i=0; i < aux; i++){
-      vetor[i] = numeroAleatorio(0, 10000);
+  if (aux <= 20){
+    for (size_t i = 0; i < aux; i++)
+      cout << vetor[i] << " ";
     }
-    sort (vetor, vetor+aux);
-    bool resposta = buscaBinaria(1101, aux, vetor);
-    if (resposta)
-      cout << "Achou" << endl;
-    else
-      cout << "Não achou" << endl;
+  cout << endl;
+  cout << "Número a ser procurado no vetor: ";
+  cin >> num;
 
-    getrusage(RUSAGE_SELF, &tempo_final);
+  getrusage(RUSAGE_SELF, &tempo_inicial);
 
-tempo = calcula_tempo(&tempo_inicial, &tempo_final);
-printf("%d\n",z);
-printf("%lf\n",tempo);
+
+  bool resposta = buscaBinaria(num, aux, vetor);
+  if (resposta)
+    cout << "Achou na busca binária" << endl;
+  else
+    cout << "Não achou na busca binária" << endl;
+
+  getrusage(RUSAGE_SELF, &tempo_final);
+
+  tempo = calcula_tempo(&tempo_inicial, &tempo_final);
+
+  getrusage(RUSAGE_SELF, &tempo_inicial);
+
+  preenche_tabela(vetor, aux, tabela_de_indices);
+
+  if(buscaIndexada(num, vetor, tabela_de_indices, aux)){
+    printf("Encontrado\n");
+  }
+  else{
+    printf("Não encontrado\n");
+  }
+  getrusage(RUSAGE_SELF, &tempo_final);
+
+  double tempo2 = calcula_tempo(&tempo_inicial, &tempo_final);
+  printf("%lf\n",tempo);
+  printf("%lf\n",tempo2);
 return 0;
 }
