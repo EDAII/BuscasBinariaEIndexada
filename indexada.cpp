f31325f503de51fd92d9a04f7e02a6ac35d023cf
joaovitorml <joao.lemos4000@gmail.com>
2018-03-22 10:21:07 -0300

    Created Indexada.cpp

--- indexada.cpp
+++ indexada.cpp
@@ -1,8 +1,12 @@
-#include <stdio.h>
-#define TAM 10
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define TAM 11
+#define TAM2 21
 
 int tabela_de_indices[TAM];
-int vetor_ordenado[20] = {3,6,8,10,12,15,25,30,33,34,40,45,46,48,50,52,56,57,60,64};
+int vetor_ordenado[TAM2] = {3,6,8,10,12,15,25,30,33,34,40,45,46,48,50,52,56,57,60,64,68};
 void preenche_tabela(){
   int i=0;
   while(i<TAM){
@@ -13,15 +17,18 @@ void preenche_tabela(){
 
 int busca(int num){
   int i=0,j=0;
-  while(num < tabela_de_indices[i] && i<TAM){
+  if(num ==0){
+    return 0;
+  }
+  while(num > tabela_de_indices[i] && i<TAM){
     i++;
   }
   if(num == tabela_de_indices[i]){
     return 1;
   }
-  else if(num > tabela_de_indices[i]){
-    j = (i*2)-2;
-    while(num > vetor_ordenado[j]){
+  else if(num < tabela_de_indices[i]){
+    j = (i*2);
+    while(num < vetor_ordenado[j]){
       j--;
     }
     if(num ==vetor_ordenado[j]){
@@ -42,14 +49,14 @@ int main(){
     cont1++;
   }
   printf("\n");
-  while(cont2<20){
+  while(cont2<TAM2){
     printf("%d\n",vetor_ordenado[cont2]);
     cont2++;
   }
 
   printf("Digite um numero: ");
   scanf("%d",&num);
-  
+
   if(busca(num)){
     printf("Encontrado\n");
   }
